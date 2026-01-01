b046f600fd6b25f22c22d50a1007034fcaa5b184
anddr28 <dronan2008@gmail.com>
2026-01-01 11:23:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.utils.toArray(".reveal").forEach(el => {
-  gsap.to(el, {
+gsap.utils.toArray(".reveal").forEach(section => {
+  gsap.to(section, {
+    scrollTrigger: {
+      trigger: section,
+      start: "top 85%",
+    },
     opacity: 1,
     y: 0,
     duration: 1,
-    ease: "power3.out",
-    scrollTrigger: {
-      trigger: el,
-      start: "top 85%",
-    }
+    ease: "power3.out"
   });
+});
+
+// параллакс фона hero
+gsap.to(".hero-bg", {
+  yPercent: 15,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
 });
